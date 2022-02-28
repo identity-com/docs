3d61cfd25082c9a7df650b7227c2856dbb354d61
Martin Riedel <web@riedel-it.de>
2022-02-28 10:49:55 -0500

    feat: updated config and moved CNAME.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Identity.com Documentation',
+  tagline: 'Decentralized Identity for everyone.',
   url: 'https://docs.identity.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
